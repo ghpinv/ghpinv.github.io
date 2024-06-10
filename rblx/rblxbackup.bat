f5a4955211eb4ccede529dbd73cd7f877bc05d52
Pinv | GitHub <57052906+ghpinv@users.noreply.github.com>
2024-06-10 14:47:03 +0900

    Update rblxbackup.bat

--- rblx/rblxbackup.bat
+++ rblx/rblxbackup.bat
@@ -72,9 +72,7 @@ if "%code%" == "update" (
 	cls
 	echo.
 	echo Updating...
-	curl https://ghpinv.github.io/rblx/rblxbackup.bat --output latest.bat
-	replace latest.bat rblxbackup.bat
-	rename latest.bat rblxbackup.bat
+	curl https://ghpinv.github.io/rblx/rblxbackup.bat --output rblxbackup.bat
 	echo.
 	rblxbackup.bat
 ) else (
